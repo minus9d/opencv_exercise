5fb373eec172bda3701547d950e27f632ae23cc1
minus9d <minus9d@gmail.com>
2014-03-03 23:06:05 +0900

    quit to use numbers directly to link libraries

--- cpp/opencv_excersize/main.cpp
+++ cpp/opencv_excersize/main.cpp
@@ -3,19 +3,31 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/ocl/ocl.hpp>
 
+// get version
+#define CV_VERSION_STR CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
+
+// add a suffix
 #ifdef _DEBUG
-#pragma comment(lib, "opencv_core248d")
-#pragma comment(lib, "opencv_highgui248d")
-#pragma comment(lib, "opencv_imgproc248d")
-#pragma comment(lib, "opencv_ocl248d")
+#define CV_EXT_STR "d.lib"
 #else
-#pragma comment(lib, "opencv_core248")
-#pragma comment(lib, "opencv_highgui248")
-#pragma comment(lib, "opencv_imgproc248")
-#pragma comment(lib, "opencv_ocl248")
+#define CV_EXT_STR ".lib"
 #endif
+
+#pragma comment(lib, "opencv_core"            CV_VERSION_STR CV_EXT_STR)
+#pragma comment(lib, "opencv_highgui"        CV_VERSION_STR CV_EXT_STR)
+#pragma comment(lib, "opencv_imgproc"  CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_calib3d"  CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_gpu"   CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_video"   CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_objdetect"  CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_features2d" CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_flann"   CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_ffmpeg"  CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_ts"   CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_contrib"  CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_ml"   CV_VERSION_STR CV_EXT_STR)
+//#pragma comment(lib, "opencv_legacy"  CV_VERSION_STR CV_EXT_STR)
 
 int main(void){
     cv::Mat img(256 * 16, 256 * 16, CV_8UC3);
