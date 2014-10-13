6cc03fb9f167b237d8aa6d29c1922d5887358473
minus9d <minus9d@gmail.com>
2014-10-13 12:06:31 +0900

    SearchPathを設定、画像を表示

--- opencv_python_exercise/opencv_python_exercise.py
+++ opencv_python_exercise/opencv_python_exercise.py
@@ -4,3 +4,11 @@ import cv2
 
 print cv2.__version__
 
+img_color = cv2.imread('../img/board.jpg')
+img_gray = cv2.imread('../img/board.jpg', cv2.CV_LOAD_IMAGE_GRAYSCALE)
+
+if img_color is not None:
+    cv2.imshow("img", img_color)
+
+cv2.waitKey()
+
